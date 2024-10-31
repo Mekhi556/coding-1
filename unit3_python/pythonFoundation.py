213ba0de88b1d9461d44ca65f5b70ee633681c44
Mekhi556 <Mturner904@boyslatin.net>
2024-10-31 12:50:21 +0000

    Started working on python

---
+++ unit3_python/pythonFoundation.py
@@ -0,0 +1,42 @@
+#data types = are the fundamental building
+# blocks of python
+
+# integers = whole numbers 
+# strings = any characters wrapped in quotes 
+# boolean= true and false 
+# we output data using the print function 
+print(200* 8)
+print('welcome to pyhton. !@#')
+print(True)
+
+() #round brackets = funtion
+{} #curly brackets = objects
+[] #square brackets = list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+name="Mekhi"
+print(message, name)
+
+#contenation = combining strings
+print(message, name)
+
+story = " i will be goinng to"
+location="Trick or Treating ="
+time='at 8pm'
+print (story)
+
+valA=10
+valA=20
+print(valA,valB)
